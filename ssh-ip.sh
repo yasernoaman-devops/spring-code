5859579b2378a65ea510cf56dc3204e1ed264df1
yasernoaman-devops <155712713+yasernoaman-devops@users.noreply.github.com>
2024-02-13 16:54:47 +0530

    Update ssh-ip.sh

--- ssh-ip.sh
+++ ssh-ip.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-IP_FILE="public_ips.txt"
 while IFS= read -r ip; do
-    echo "Executing command on $ip"
-	
-    ssh-keygen -f "/home/azureuser/.ssh/known_hosts" -R "$ip"
-	ssh -o "StrictHostKeyChecking=no" $ip" 
-done < "$IP_FILE"
+    echo "Executing commands on $ip"
+    'ssh-keygen -f "/home/azureuser/.ssh/known_hosts" -R "$ip"; ssh -o "StrictHostKeyChecking=no" $ip'
+done < "$1"	
+echo "Script execution completed."
