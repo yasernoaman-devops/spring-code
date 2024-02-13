112073cea7609e9c09ff7ba3825e8f51764b5f0d
yasernoaman-devops <155712713+yasernoaman-devops@users.noreply.github.com>
2024-02-13 18:12:27 +0530

    Update ssh-ip.sh

--- ssh-ip.sh
+++ ssh-ip.sh
@@ -1,6 +1,8 @@
+
 #!/bin/bash
+file_path="terrform-code/public_ips.txt"
 while IFS= read -r ip; do
     echo "Executing commands on $ip"
     'ssh-keygen -f "/home/azureuser/.ssh/known_hosts" -R "$ip"; ssh -o "StrictHostKeyChecking=no" $ip'
-done < "$1"	
+done < "$file_path"	
 echo "Script execution completed."
