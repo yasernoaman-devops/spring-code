c1c77bfe74f4ee85e0adf6113adbaf877eb6da02
yasernoaman-devops <155712713+yasernoaman-devops@users.noreply.github.com>
2024-02-15 19:24:43 +0530

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -2,7 +2,7 @@
 file_path="terrform-code/public_ips.txt"
 while IFS= read -r ip; do
     echo "Executing commands on $ip"
-               ssh azureuser@$ip 'docker run -itd --name tomcat-container acrnoaman.azurecr.io/abha:${{ github.run_number }}-DEV'
+               ssh azureuser@$ip 'yum install java -y; yum install docker -y; docker run -itd --name tomcat-container acrnoaman.azurecr.io/abha:${{ github.run_number }}-DEV'
 
 done < ""$file_path""
 
