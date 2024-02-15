7f6398c8f9bfc7d29868a747e63b58e108fb8700
yasernoaman-devops <155712713+yasernoaman-devops@users.noreply.github.com>
2024-02-15 20:12:46 +0530

    Update vm.tf

--- terrform-code/vm.tf
+++ terrform-code/vm.tf
@@ -1,37 +1,28 @@
-############
-
-resource "azurerm_virtual_machine" "linuxvm" {
+resource "azurerm_linux_virtual_machine" "linuxvm" {
         count  = var.vm_count
         name   = "${var.vm_prefix}-${count.index}"
-        vm_size = "Standard_DS12_v2"
+        size = "Standard_DS12_v2"
         network_interface_ids = [azurerm_network_interface.main-network-interface[count.index].id]
-        delete_data_disks_on_termination = "true"
-        delete_os_disk_on_termination = "true"
+        disable_password_authentication = "true"
         resource_group_name = "PRACTICE-RG"
+        admin_username        = "azureuser"
         location = var.location-1
-        storage_os_disk {
+        admin_ssh_key {
+          username   = "azureuser"
+          public_key = file("~/.ssh/id_rsa.pub")  # Replace with the path to your public key
+        }
+        os_disk {
             name              = "myOsDisk-${count.index}"
             caching           = "ReadWrite"
-            create_option     = "FromImage"
-            managed_disk_type = "Standard_LRS"
+            storage_account_type = "Standard_LRS"
         }
-        os_profile_linux_config {
-            disable_password_authentication = true
-            ssh_keys {
-            path     = "/home/abdmuser/.ssh/authorized_keys"
-            key_data = "/home/abdmuser/.ssh/id_rsa.pub"
-            }
-        }   
-        storage_image_reference {
-              id =  "/subscriptions/dc60d2a6-ae24-4960-a488-f53a8643a853/resourceGroups/PRACTICE-RG/providers/Microsoft.Compute/galleries/yaser/images/honeywell"
-        }
-
+        source_image_reference {
+           publisher = "Canonical"
+           offer     = "0001-com-ubuntu-server-jammy"
+           sku       = "22_04-lts"
+           version   = "latest"
+                }
         provisioner "local-exec" {
               command = "echo ${azurerm_public_ip.main-public-ip[count.index].ip_address} >> public_ips.txt"
-  }          
+  }
 }
-
-
-###########
-
-
