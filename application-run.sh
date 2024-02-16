361254ee177c91504ee0b676476e1dc5eccd1328
yasernoaman-devops <155712713+yasernoaman-devops@users.noreply.github.com>
2024-02-16 10:53:00 +0530

    Update application-run.sh

--- application-run.sh
+++ application-run.sh
@@ -1,9 +1,30 @@
 #!/bin/bash
-file_path="terrform-code/public_ips.txt"
-while IFS= read -r ip; do
-    echo "Executing commands on $ip"
-               ssh azureuser@$ip 'sudo docker login -u ACRNOAMAN -p BvZYFM64ATh/HjrSJm6mAMk0/qM9PVLvMhss2TeuSM+ACRDF7GJU  acrnoaman.azurecr.io; sudo docker pull acrnoaman.azurecr.io/abha:${{ github.run_number }}-DEV; sudo docker run -itd --name demo-container -p 80:8080 acrnoaman.azurecr.io/abha:${{ github.run_number }}-DEV'
 
-done < ""$file_path""
+# Replace these values with your ACR credentials
+ACR_NAME="ACRNOAMAN"
+ACR_USERNAME="ACRNOAMAN"
+ACR_PASSWORD="BvZYFM64ATh/HjrSJm6mAMk0/qM9PVLvMhss2TeuSM+ACRDF7GJU"
 
-echo "Script execution completed."
+# Replace this with the path to your test file containing IP addresses
+TEST_FILE="terrform-code/public_ips.txt"
+
+# Replace this with the desired image name
+IMAGE_NAME="abha"
+
+# Get GitHub run number as the image tag
+GITHUB_RUN_NUMBER=$1
+
+# Log in to ACR
+
+
+# Iterate over IP addresses from the test file
+while IFS= read -r IP_ADDRESS; do
+    echo "Processing $IP_ADDRESS"
+    ssh azureuser@$IP_ADDRESS 'az acr login --name $ACR_NAME --username $ACR_USERNAME --password $ACR_PASSWORD; docker pull $ACR_NAME.azurecr.io/$IMAGE_NAME:$GITHUB_RUN_NUMBER-DEV'
+    # Run other commands or operations as needed for each IP address
+    # For example, you can deploy the pulled image to the specified IP address
+
+done < "$TEST_FILE"
+
+# Logout from ACR
+az acr logout
