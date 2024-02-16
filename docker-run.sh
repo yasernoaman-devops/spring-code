71c48576d0f84be2e428870149025aef341b31e1
yasernoaman-devops <155712713+yasernoaman-devops@users.noreply.github.com>
2024-02-16 13:54:33 +0530

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -2,7 +2,7 @@
 file_path="terrform-code/public_ips.txt"
 while IFS= read -r ip; do
     echo "Executing commands on $ip"
-               ssh azureuser@$ip 'sudo apt update; sudo apt install default-jre -y; sudo apt install apt-transport-https ca-certificates curl software-properties-common -y; curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -; sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y;  sudo apt install docker-ce -y'
+               ssh azureuser@$ip 'sudo apt update; sudo apt-get install -y azure-cli; sudo apt install default-jre -y; sudo apt install apt-transport-https ca-certificates curl software-properties-common -y; curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -; sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y;  sudo apt install docker-ce -y'
 
 done < ""$file_path""
 
