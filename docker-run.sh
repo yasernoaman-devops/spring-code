ba6ecaef97f593b66637ccb44947874473e2f8d9
yasernoaman-devops <155712713+yasernoaman-devops@users.noreply.github.com>
2024-02-16 17:21:36 +0530

    Update docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-file_path="terrform-code/public_ips.txt"
-while IFS= read -r ip; do
-    echo "Executing commands on $ip"
-               ssh azureuser@$ip '
 
+# File containing IP addresses
+ip_file="terrform-code/public_ips.txt"
+
+# Read each line from the file
+while IFS= read -r ip_address; do
+    echo "Connecting to $ip_address..."
+
+    # SSH into the remote server and execute commands
+    ssh azureuser@$ip_address <<'EOF'
+        # Commands to be executed on the remote server
+          echo "Executing commands on $HOSTNAME"
+          sudo  az acr login --name ACRNOAMAN --username ACRNOAMAN --password BvZYFM64ATh/HjrSJm6mAMk0/qM9PVLvMhss2TeuSM+ACRDF7GJU
           curl -sL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/microsoft.gpg > /dev/null
           echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/azure-cli.list
           sudo apt update
@@ -14,8 +22,16 @@ while IFS= read -r ip; do
           sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable" -y
           sudo apt install docker-ce -y
 
-               '
 
-done < ""$file_path""
 
-echo "Script execution completed."
+EOF
+
+    # Check the exit status of the SSH command
+    if [ $? -eq 0 ]; then
+        echo "Successfully executed commands on $ip_address"
+    else
+        echo "Error: Failed to execute commands on $ip_address"
+    fi
+
+done < "$ip_file"
+
