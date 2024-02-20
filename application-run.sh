e85ad92c50d2dbc6e776955bc4462e1bb5108f72
yasernoaman-devops <155712713+yasernoaman-devops@users.noreply.github.com>
2024-02-20 09:29:25 +0530

    Update application-run.sh

--- application-run.sh
+++ application-run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
-# File containing IP addresses
 ip_file="terrform-code/public_ips.txt"
-# Read each line from the file
 while IFS= read -r ip_address; do
     echo "Connecting to $ip_address..."
-
-    # SSH into the remote server and execute commands
-    ssh azureuser@$ip_address <<'EOF'
-        GH_RUN_NUMBER=$(echo "${GITHUB_RUN_NUMBER}" | tr -d -)
-        # Commands to be executed on the remote server
+    GH_RUN_NUMBER=$(echo "${GITHUB_RUN_NUMBER}" | tr -d -)
+    ssh azureuser@$ip_address <<EOF
+        echo $GH_RUN_NUMBER
         echo "Executing commands on $HOSTNAME"
         sudo az acr login --name HACIAC --username HACIAC --password /bfp1M4tMSg5BXNdXC6WwGar7ecj+9m0E/87OQmNz9+ACRBQuFOH
         sudo docker pull haciac.azurecr.io/hce:$GH_RUN_NUMBER-DEV
@@ -17,8 +12,6 @@ while IFS= read -r ip_address; do
 
 
 EOF
-
-    # Check the exit status of the SSH command
     if [ $? -eq 0 ]; then
         echo "Successfully executed commands on $ip_address"
     else
